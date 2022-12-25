4a322ec396517b3d7e9ed743b6d27f360db90dfe
corriolgamer@gmail.com <corriolgamer@gmail.com>
2022-12-25 21:57:59 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-ReactDOM.render(<h1>Привет</h1>, document.getElementById("app"))
+const placeholder = () => { return 'Hello, world!' }
+
+const elements = <input placeholder = {placeholder}/>
+const app = document.getElementById("app")
+
+ReactDOM.render(elements, app)
