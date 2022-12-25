fc5522d47d7a6e3025c9bbc4c0a60f482d7a2cea
corriolgamer@gmail.com <corriolgamer@gmail.com>
2022-12-25 22:00:25 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const placeholder = () => { return 'Hello, world!' }
 
-const elements = <input placeholder={placeholder}/>
+const elements = <input placeholder="Hello"/>
 const app = document.getElementById("app")
 
 ReactDOM.render(elements, app)
