4b82f1bc6d02f187e8089f84f80dbc35e0417e15
corriolgamer@gmail.com <corriolgamer@gmail.com>
2022-12-25 22:44:36 +0200

    1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+const placeholder = "Hello, world!"
+
 class App extends React.Components {
-  placeholder = "Hello, world!"
+  
   render() {
     return (
-      <input placeholder={this.placeholder}/>
+      <input placeholder={placeholder}/>
     )
   }
 }
